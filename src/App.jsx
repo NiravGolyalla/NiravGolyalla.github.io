dc43e941fb93e88c8ae68499b7e8cd9b739c8c34
nirav-golyalla <ng2411@nyu.edu>
2023-08-16 21:40:02 -0400

    Testing Dynamic pages on Github

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='App'>
+      <button onClick={() => setCount((prevCount) => prevCount -1)}>-</button>
+      <p>{count}</p>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
+    </div>
+  )
+}
+
+export default App
